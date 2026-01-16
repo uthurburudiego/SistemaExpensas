9f00bf2755f34f313ca7f608a0f13a859a144ae5
diego uthurburu <diegouthurburulima@gmail.com>
2026-01-15 21:08:37 -0300

    Fix: removed static frontend serving

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db';
-import loteRoutes from './routes/loteRoutes'; // Importa las rutas
+import loteRoutes from './routes/loteRoutes'; 
 import authRoutes from './routes/authRoutes';
-import path from 'path';
+// Ya no necesitas 'path' si no vas a servir el frontend desde aquí
 
 const app = express();
 app.use(express.json());
 
-// Configurar CORS para permitir que Vite (puerto 5173) se comunique con el servidor
 app.use(cors({
-  origin: '*', // Permite solo a tu frontend de Vite
+    origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
-
 
 // Conectar a la DB
 connectDB();
@@ -23,19 +21,8 @@ connectDB();
 app.use('/api/lotes', loteRoutes);
 app.use('/api/auth', authRoutes);
 
-
 // Iniciar el servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
 });
-
-// Servir archivos estáticos en producción
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/dist')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client/dist/index.html'))
-  );
-}
